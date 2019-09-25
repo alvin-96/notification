7caff0c38b487e7c34b41954f5831af3027931e8
Alvin <alvin@fqdn.example.com>
2019-09-25 16:28:34 +0800

    testing

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,7 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+  'messagingSenderId': '211236340357'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
